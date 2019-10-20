1e22d654070f561b4cd0091232aeabe9fcb7a751
Samuel Schlesinger <samuel@simspace.com>
2019-10-20 15:08:51 -0400

    Stopped working because I was having some problems with the Bootstrap navbar.. Gonna learn about the Erlang RTS instead

--- samuelschlesinger-info-server/src/FrontEnd.hs
+++ samuelschlesinger-info-server/src/FrontEnd.hs
@@ -29,6 +29,13 @@ popper_1_14_7 :: H.Html
 popper_1_14_7 = do
   H.script ! A.src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" $ pure ()
 
+layout :: H.Html -> H.Html
+layout middle = do
+  H.div ! A.class_ "jumbotron text-center" $ do
+    H.h1 "Samuel Schlesinger"
+    H.p "Researcher and software engineer" 
+  middle
+
 includes :: H.Html
 includes = do
   bootstrap_4_3_1
@@ -41,10 +48,48 @@ home = H.docTypeHtml $ do
     H.title "Samuel Schlesinger's Homepage"
     metas
     includes
-    
+  H.body $ layout $ do
+    H.nav ! A.class_ "navbar" ! A.class_ "navbar-default" $ do
+      H.div ! A.class_ "container-fluid" $ do
+        H.div ! A.class_ "navbar-header" $ do
+          H.a ! A.class_ "navbar-brand" ! A.href "#" $ "samuelschlesinger.info"
+        H.ul ! A.class_ "nav" ! A.class_ "navbar-nav" $ do
+          H.li ! A.class_ "active" $ H.a ! A.href "#" $ "About Me"
+          H.li                     $ H.a ! A.href "/contact" $ "Contact Me"
+    H.div ! A.class_ "container" $ do
+      H.div ! A.class_ "row" $ do
+        H.div ! A.class_ "col-sm-6" $ do
+          H.h3 "Professional Background"
+          H.p "I am a 24 year old software engineer at SimSpace Corporation, working \
+              \in the Haskell programming language. Technically, my interests lie in \
+              \functional programming, computational complexity theory, artificial \
+              \intelligence, algorithm design, computational logic, and I conduct \
+              \independent research into all of these areas, as well as research for \
+              \work when it is required of me. I have a strong interest in teaching \
+              \and helping others learn. If you ever want to reach out to me about \
+              \any of these topics, please feel free to do so."
+        H.div ! A.class_ "col-sm-6" $ do
+          H.h3 "Personal Background"
+          H.p "I grew up in Massachusetts, in a small town by the beach. \
+              \I have always been interested in analytic and continental \
+              \philosophy, and I consider my interest in mathematics and \
+              \computer science an extension of my interest in analytic \
+              \philosophy. I am also interested in many other things, such \
+              \as geopolitics, outdoor activities, writing short fiction, \
+              \fitness, and a number of other areas."
+
 contact :: H.Html
 contact = H.docTypeHtml $ do
   H.head $ do
     H.title "Contact Samuel Schlesinger"
     metas
     includes
+  H.body $ layout $ do
+    H.nav ! A.class_ "navbar" ! A.class_ "navbar-default" $ do
+      H.div ! A.class_ "container-fluid" $ do
+        H.div ! A.class_ "navbar-header" $ do
+          H.a ! A.class_ "navbar-brand" ! A.href "#" $ "samuelschlesinger.info"
+        H.ul ! A.class_ "nav" ! A.class_ "navbar-nav" $ do
+          H.li                     $ H.a ! A.href "/" $ "About Me"
+          H.li ! A.class_ "active" $ H.a ! A.href "#" $ "Contact Me"
+    pure ()
